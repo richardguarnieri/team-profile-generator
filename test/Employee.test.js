b6357dbabdd75a4aa9c801db35e88a7a35eecbd1
Richard Guarnieri <richard.gm@outlook.com>
2022-05-17 11:26:17 -0500

    completed Employee test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,6 +1,7 @@
 const Employee = require('../lib/Employee')
 
 describe('Employee', () => {
+    // Constructor Test
     describe('Initialization', () => {
         describe('name', () => {
             // Positive Test
@@ -16,7 +17,7 @@ describe('Employee', () => {
             it("Should throw an error if not provided a 'string' value", () => {
                 // Arrange
                 const cb = () => new Employee(1, 31, '@example.com')
-                const err = new Error("Expected parameter 'name' to be a string");
+                const err = new Error("Expected parameter 'name' to be a non-empty string");
                 // Assert
                 expect(cb).toThrowError(err);
             });
@@ -43,7 +44,7 @@ describe('Employee', () => {
             it("Should throw an error if not provided a 'number' value", () => {
                 // Arrange
                 const cb = () => new Employee('richard', '31', '@example.com')
-                const err = new Error("Expected parameter 'id' to be a number");
+                const err = new Error("Expected parameter 'age' to be a non-negative number");
                 // Assert
                 expect(cb).toThrowError(err);
             });
@@ -60,28 +61,86 @@ describe('Employee', () => {
             // Positive Test
             it("Should create an object with a 'email' property set to the 'email' argument provided when called with the 'new' keyword", () => {
                 // Arrange
-                const email = 31
+                const email = '@example.com'
                 // Act
                 const employee = new Employee('richard', 31, '@example.com')
                 // Assert
                 expect(employee.email).toBe(email);
             });
-            // Exception Test # 1
-            it("Should throw an error if not provided a 'number' value", () => {
-                // Arrange
-                const cb = () => new Employee('richard', 31, '@example.com')
-                const err = new Error("Expected parameter 'email' to be a number");
-                // Assert
-                expect(cb).toThrowError(err);
-            });
-            // Exception Test # 2
-            it("Should throw an error if provided a negative 'number' value", () => {
-                // Arrange
-                const cb = () => new Employee('richard', 31, '@example.com')
-                const err = new Error("Expected parameter 'age' to be a non-negative number");
-                // Assert
-                expect(cb).toThrowError(err);
-            });
+            // // Exception Test # 1
+            // it("Should throw an error if not provided a 'number' value", () => {
+            //     // Arrange
+            //     const cb = () => new Employee('richard', 31, '@example.com')
+            //     const err = new Error("Expected parameter 'email' to be a number");
+            //     // Assert
+            //     expect(cb).toThrowError(err);
+            // });
+            // // Exception Test # 2
+            // it("Should throw an error if provided a negative 'number' value", () => {
+            //     // Arrange
+            //     const cb = () => new Employee('richard', 31, '@example.com')
+            //     const err = new Error("Expected parameter 'age' to be a non-negative number");
+            //     // Assert
+            //     expect(cb).toThrowError(err);
+            // });
         });
     });
+    
+    // Methods Test
+    describe('Methods', () => {
+        describe('getName', () => {
+            // Positive Test
+            it('Should return the name of the object\'s instance', () => {
+                // Arrange
+                const name = 'richard';
+                // Act
+                const employee = new Employee('richard', 31, '@example.com');
+                const employeeName = employee.getName();
+                // Assert
+                expect(employeeName).toBe(name);
+            });
+        })
+        describe('getId', () => {
+            // Positive Test
+            it('Should return the id of the object\'s instance', () => {
+                // Arrange
+                const id = 31;
+                // Act
+                const employee = new Employee('richard', 31, '@example.com');
+                const employeeId = employee.getId();
+                // Assert
+                expect(employeeId).toBe(id);
+            });
+        })
+        describe('getEmail', () => {
+            // Positive Test
+            it('Should return the email of the object\'s instance', () => {
+                // Arrange
+                const email = '@example.com';
+                // Act
+                const employee = new Employee('richard', 31, '@example.com');
+                const employeeEmail = employee.getEmail();
+                // Assert
+                expect(employeeEmail).toBe(email);
+            });
+        })
+        describe('getRole', () => {
+            // Positive Test
+            it("Should return 'Employee'", () => {
+                // Arrange
+                const role = 'Employee';
+                // Act
+                const employee = new Employee('richard', 31, '@example.com');
+                const employeeRole = employee.getRole();
+                // Assert
+                expect(employeeRole).toBe(role);
+            });
+        })
+    })
+
+
+
+
+
+
 });
