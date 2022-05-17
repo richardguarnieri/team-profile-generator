5a34ecacecfdd49ef66c0e13b443cf0b76e01b80
Richard Guarnieri <richard.gm@outlook.com>
2022-05-17 12:06:30 -0500

    added Manager.test.js

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -22,11 +22,38 @@ describe('Manager', () => {
                 // Assert
                 expect(manager.officeNumber).toBe(officeNumber);
             });
+            // Exception Test # 1
+            it("Should throw an error if not provided a 'string' value", () => {
+                // Arrange
+                const cb = () => new Manager('richard', 31, '@example.com', 1)
+                const err = new Error("Expected parameter 'officeNumber' to be a non-empty string");
+                // Assert
+                expect(cb).toThrowError(err);
+            });
+            // Exception Test # 2
+            it("Should throw an error if provided an empty 'string' value", () => {
+                // Arrange
+                const cb = () => new Employee('richard', 31, '@example.com', ' ')
+                const err = new Error("Expected parameter 'officeNumber' to be a non-empty string");
+                // Assert
+                expect(cb).toThrowError(err);
+            });
         });
     });
     
     // Methods Test
-
-
-
+    describe('Methods', () => {
+        describe('getRole', () => {
+            // Positive Test
+            it("Should return 'Manager'", () => {
+                // Arrange
+                const role = 'Manager';
+                // Act
+                const manager = new Manager('richard', 31, '@example.com', '5511119999');
+                const managerRole = manager.getRole();
+                // Assert
+                expect(managerRole).toBe(role);
+            });
+        })
+    })
 });
